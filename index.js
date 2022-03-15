6a281a0f6a04a35a22d304df0d42ba50166003b2
+ <+>
2022-03-15 12:26:28 -0400

    commit tree 413418265bc413086111509c29af1e0e0bd7a6c1

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i ++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j <array.length; j++) {
+      if (array[j] === complement) return true
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n^2)
+  Space: O(n)
 */
 
 /* 
-  Add your pseudocode here
+For each number in the array, 
+subtract from the target and then 
+check the array for the difference. 
 */
 
 /*
-  Add written explanation of your solution here
+This function uses a for loop to subtract
+each number in the array
+from the target to find the difference. 
+Then it check to see if the difference is in the array.
+If so, it returns true. 
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4, 5, 6], 6));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
